97726278866bd9d7c928831e727a5a012c4eee62
Leanderson-Machado-Moraes <lele.momo245@gmail.com>
2022-04-12 18:41:36 -0300

    Lâmpada

---
+++ js/codigo.js
@@ -0,0 +1,34 @@
+const lampada = document.getElementById("lampada")
+const azul = document.getElementById("azul")
+const vermelho = document.getElementById("vermelho")
+const laranja = document.getElementById("laranja")
+const amarelo = document.getElementById("amarelo")
+const verde = document.getElementById("verde")
+const branco = document.getElementById("branco")
+const power = document.getElementById("ligadesliga")
+
+function desligareligar(){
+    if(lampada = img.src="./img/"){
+        lampada = img.src="./img/"
+    }else{
+        lampada = img.src="./img"
+    }
+}
+function corazul(){
+    lampada.src="./img/"
+}
+function coramarelo(){
+    lampada.src="./img/"
+}
+function corlaranja(){
+    lampada.src="./img/"
+}
+function corvermelha(){
+    lampada.src="./img/"
+}
+function corbranca(){
+    lampada.src="./img/"
+}
+function corverde(){
+    lampada.src="./img/"
+}
